7c0eca2f23fc8bd3aeac3b65115c1cf228fe1eca
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2024-02-28 00:30:18 -0300

    setting files

---
+++ src/main/java/com/marcelinx/consultadentista/ConsultaDentistaApplication.java
@@ -0,0 +1,13 @@
+package com.marcelinx.consultadentista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaDentistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaDentistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcelinx/consultadentista/ConsultaDentistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelinx.consultadentista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaDentistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
